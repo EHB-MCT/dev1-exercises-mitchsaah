4520864d06732ce94b0c84cf87e061ce9a7aae82
Mitch Saah <mitch.saah@student.ehb.be>
2022-10-05 12:18:37 +0200

    starry night in progress

--- exercises/1-functions/1-5b-ellipse.js
+++ exercises/1-functions/1-5b-ellipse.js
@@ -1,8 +1,8 @@
 "use strict";
 
-drawSunset();
+drawEllipse();
 
-function drawSunset() {
+function drawEllipse() {
 
     let canvas = document.querySelector("canvas");
     canvas.width = window.innerWidth;
@@ -15,16 +15,16 @@ function drawSunset() {
     context.rect(100, 100, 400, 400);
     context.fill();
 
+    //Ellipse
+    context.fillStyle = "#ffff01";
+    context.beginPath();
+    context.ellipse(300, 350, 100, 50, 0, 0, Math.PI * 2, true);
+    context.fill();
+
     //Water
     context.fillStyle = "#0000fe";
     context.beginPath();
     context.rect(100, 350, 400, 150);
-    context.fill();
-
-    //Ellipse
-    context.fillStyle = "#ffff01";
-    context.beginPath();
-    context.arc(300, 350, 100, 0, Math.PI, true);
     context.fill();
 
 }
---
+++ exercises/1-functions/1-6-starry-night.js
@@ -0,0 +1,31 @@
+"use strict";
+
+drawStarryNight();
+
+function drawStarryNight() {
+
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    context.lineWidth = "5";
+    context.strokeStyle = "#fda704";
+    
+    //Frame
+    context.beginPath();
+    context.rect(100, 100, 400, 400);
+    context.stroke();
+
+    //Star
+    context.beginPath();
+    context.arc(100, 100, 200, 0, Math.PI /2);
+    context.arc()
+    //context.arc();
+    
+    context.stroke();
+
+
+
+
+}
